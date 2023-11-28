d5b64a61da47b8a1fd6f69017558757e8ad74fd7
Arthur Louvrier <arthur.louvrier@etu.minesparis.psl.eu>
2023-11-28 11:45:38 +0100

    Fixed big issue lol

--- main.py
+++ main.py
@@ -3,22 +3,32 @@ import numpy.random as rd
 import matplotlib.pyplot as plt
 
 print("Vizualization of Ginibre eigenvalues and Saturn effect")
-
-N = 3000
-mu = 0.0
-sigma = 1.0/np.sqrt(2*N)
-
 gen = rd.default_rng()
-mat = gen.normal(mu,sigma, (N,N))+gen.normal(mu,sigma,(N,N))*complex(0,1)
-print(mat)
+N = 16
+mu = 0.0
+sigma = 1.0/np.sqrt(1*N)
 
-eigenvalues, _= np.linalg.eig(mat)
+def obtainGinibreEig(N,mu,sigma):
+    mat = gen.normal(mu,sigma, (N,N)) #+gen.normal(mu,sigma,(N,N))*complex(0,1)
+    #print(mat)
+
+    eigenvalues, _= np.linalg.eig(mat)
+    #print(eigenvalues)
+
+
+    #plt.xlim(-1.0,1.0)
+    #plt.ylim(-1.0,1.0)
+    return eigenvalues
+
+eigenvalues = []
+for _ in range(1000):
+    eigenvalues.append(obtainGinibreEig(N,mu,1.0))
+    
 print(eigenvalues)
+values = np.append([], eigenvalues)
+print(values)
 
-
-#plt.xlim(-1.0,1.0)
-#plt.ylim(-1.0,1.0)
 plt.gca().set_aspect('equal')
-plt.scatter(eigenvalues.real, eigenvalues.imag, s = 2,c="red")
+plt.scatter(values.real, values.imag, s = 1,c="red")
 plt.title("Eigenvalues of Ginibre matrix")
 plt.show()
